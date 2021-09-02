3d184b9ec19f6cd2f21c135b9f1304fc9caaceb4
donnyh0730 <donnyh0730@gmail.com>
2021-09-02 13:26:21 +0900

    Inclding TypeCast Outer Library in project

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -9,65 +9,95 @@
 #include "Memory.h"
 #include "RefCounting.h"
 #include "Allocator.h"
-class Player
-{
 
+using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
+
+class Player 
+{
+public:
+	Player()
+	{
+		INIT_TL(Player);
+	}
+	virtual ~Player() { }
+
+	DECLARE_TL
 };
 
 class Knight : public Player
 {
 public:
-	Knight()
-	{
-		cout << "Kinght()" << endl;
-	}
-	Knight(int32 _hp)
-		:hp(_hp)
-	{
-		cout << "Kinght() int32" << endl;		
-	}
-	~Knight()
-	{
-		cout << "~Kinght()" << endl;
-	}
-
-	int32 hp = 100;
-	int32 mp = 10;
+	Knight() { INIT_TL(Knight); }
 };
 
-class Monster
+class Mage : public Player
 {
+
 public:
-	int8 id;
+	Mage() { INIT_TL(Mage); }
 };
 
+class Archer : public Player
+{
+
+public:
+	Archer() { INIT_TL(Archer) }
+};
+
+class Dog
+{
+
+};
 int main()
 {
-	Knight* knights[100];
-	for (int32 i = 0; i < 100; ++i)
-	{
-		knights[i] = ObjectPool<Knight>::Pop();
-	}
-	for (int32 i = 0; i < 100; ++i)
-	{
-		ObjectPool<Knight>::Push(knights[i]);
-		knights[i] = nullptr;
-	}
+	//TypeList<Mage, Knight>::Head whoAMI;
+	//TypeList<Mage, Knight>::Tail whoAMI2;
 
-	shared_ptr<Knight> sptr = ObjectPool<Knight>::MakeShared();
-	shared_ptr<Knight> sptr2 = MakeShared<Knight>();
+	//TypeList<Mage, TypeList<Knight, Archer>>::Head whoAMI3;
+	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Head whoAMI4;
+	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Tail whoAMI5;
 
+	//int32 len1 = Length<TypeList<Mage, Knight>>::value;
+	//int32 len2 = Length<TypeList<Mage, Knight, Archer>>::value;
+	//
+	//
+	//// 3*3 
+	//TypeAt<TL, 0>::Result whoAMI6;
+	//TypeAt<TL, 1>::Result whoAMI7;
+	//TypeAt<TL, 2>::Result whoAMI8;
+
+	//int32 index1 = IndexOf<TL, Mage>::value;
+	//int32 index2 = IndexOf<TL, Archer>::value;
+	//int32 index3 = IndexOf<TL, Dog>::value;
+
+	//bool canConvert1 = Conversion<Player, Knight>::exists;
+	//bool canConvert2 = Conversion<Knight, Player>::exists;
+	//bool canConvert3 = Conversion<Knight, Dog>::exists;
+
+	/*{
+		Player* player = new Knight();
+
+		bool canCast = CanCast<Knight*>(player);
+		Knight* knight = TypeCast<Knight*>(player);
+
+
+		delete player;
+	}*/
+
+	{
+		shared_ptr<Player> player = MakeShared<Knight>();
+
+		shared_ptr<Archer> archer = TypeCast<Archer>(player);
+		bool canCast = CanCast<Mage>(player);
+
+	}
 	for (int32 i = 0; i < 5; ++i)
 	{
 		GThreadManager->Launch([]()
 			{
 				while (true)
 				{
-					Vector<Knight> v(100);
-
-					Map<int32, Knight> m;
-					m[100] = Knight();
-					this_thread::sleep_for(10ms);
+					
 				}
 
 			});
