d9235f7bdfbdd8458f5dffd17b6ee7c0e75fd790
donnyh0730 <donnyh0730@gmail.com>
2021-09-27 13:51:29 +0900

    Additional Session function #1

--- Server/ServerCore/Session.cpp
+++ Server/ServerCore/Session.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
 #include "Session.h"
 #include "SocketUtils.h"
+#include "Service.h"
+
+/*--------------
+	Session
+---------------*/
 
 Session::Session()
 {
@@ -12,6 +17,19 @@ Session::~Session()
 	SocketUtils::Close(_socket);
 }
 
+void Session::Disconnect(const WCHAR* cause)
+{
+	if (_connected.exchange(false) == false)
+		return;
+
+	// TEMP
+	wcout << "Disconnect : " << cause << endl;
+
+	OnDisconnected(); // 컨텐츠 코드에서 오버로딩
+	SocketUtils::Close(_socket);
+	GetService()->ReleaseSession(GetSessionRef());
+}
+
 HANDLE Session::GetHandle()
 {
 	return reinterpret_cast<HANDLE>(_socket);
@@ -19,5 +37,102 @@ HANDLE Session::GetHandle()
 
 void Session::Dispatch(IocpEvent* iocpEvent, int32 numOfBytes)
 {
+	switch (iocpEvent->eventType)
+	{
+	case EventType::Connect:
+		ProcessConnect();
+		break;
+	case EventType::Recv:
+		ProcessRecv(numOfBytes);
+		break;
+	case EventType::Send:
+		ProcessSend(numOfBytes);
+		break;
+	default:
+		break;
+	}
+}
+
+void Session::RegisterConnect()
+{
+}
+
+void Session::RegisterRecv()
+{
+	if (IsConnected() == false)
+		return;
+
+	_recvEvent.Init();
+	_recvEvent.owner = shared_from_this(); // ADD_REF
+
+	WSABUF wsaBuf;
+	wsaBuf.buf = reinterpret_cast<char*>(_recvBuffer);
+	wsaBuf.len = len32(_recvBuffer);
+
+	DWORD numOfBytes = 0;
+	DWORD flags = 0;
+	//비동기로 입출력을 걸어주므로, 버퍼의 입출력이 완료되는 것은 컴플리션 포트에서 일감이 빠져나갔을때이다. 
+	if (SOCKET_ERROR == ::WSARecv(_socket, &wsaBuf, 1, OUT & numOfBytes, OUT & flags, &_recvEvent, nullptr))
+	{
+		int32 errorCode = ::WSAGetLastError();
+		if (errorCode != WSA_IO_PENDING)
+		{
+			HandleError(errorCode);
+			_recvEvent.owner = nullptr; // RELEASE_REF
+		}
+	}
+}
+
+void Session::RegisterSend()
+{
+}
+
+void Session::ProcessConnect()
+{
+	_connected.store(true);
+
+	// 세션 등록
+	GetService()->AddSession(GetSessionRef());
+
+	// 컨텐츠 코드에서 오버로딩
+	OnConnected();
+
+	// 수신 등록
+	RegisterRecv();
+}
+
+void Session::ProcessRecv(int32 numOfBytes)
+{
+	_recvEvent.owner = nullptr; // RELEASE_REF
+
+	if (numOfBytes == 0)
+	{
+		Disconnect(L"Recv 0");
+		return;
+	}
+
 	// TODO
+	cout << "Recv Data Len = " << numOfBytes << endl;
+
+	// 수신 등록
+	RegisterRecv();
+}
+
+void Session::ProcessSend(int32 numOfBytes)
+{
+}
+
+void Session::HandleError(int32 errorCode)
+{
+	switch (errorCode)
+	{
+	case WSAECONNRESET:
+	case WSAECONNABORTED:
+		Disconnect(L"HandleError");
+		break;
+	default:
+		// TODO : Log
+		cout << "Handle Error : " << errorCode << endl;
+		break;
+	}
 }
