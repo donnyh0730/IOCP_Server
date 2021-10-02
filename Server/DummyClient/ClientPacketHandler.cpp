782692537d1b52c1a7d568422a3843c5001d4731
donnyh0730 <donnyh0730@gmail.com>
2021-10-02 17:51:09 +0900

    Packet Serialization #2 (Flat buffer Packet Handler idea)

--- Server/DummyClient/ClientPacketHandler.cpp
+++ Server/DummyClient/ClientPacketHandler.cpp
@@ -36,7 +36,7 @@ struct PKT_S_TEST
 	uint16 buffsOffset;
 	uint16 buffsCount;
 
-	bool Validate()
+	bool IsValidePck()
 	{
 		uint32 size = 0;
 		size += sizeof(PKT_S_TEST);
@@ -49,8 +49,15 @@ struct PKT_S_TEST
 
 		return true;
 	}
-	//vector<BuffData> buffs;
-	//wstring name;
+	using BuffsList = PacketList<PKT_S_TEST::BuffsListItem>;
+
+	BuffsList GetBuffsList()
+	{
+		BYTE* data = reinterpret_cast<BYTE*>(this);
+		data += buffsOffset;//18바이트 만큼 이동 
+		return BuffsList(reinterpret_cast<PKT_S_TEST::BuffsListItem*>(data), buffsCount);
+		//이 위치에서 포인터 캐스팅을하면 버프데이터 배열을 얻을 수 있다.
+	}
 };
 #pragma pack()
 
@@ -58,26 +65,31 @@ void ClientPacketHandler::Handle_S_TEST(BYTE* buffer, int32 len)
 {
 	BufferReader br(buffer, len);
 
-	if (len < sizeof(PKT_S_TEST))
-		return;
-
-	PKT_S_TEST pkt;
-	br >> pkt;
-
-	if (pkt.Validate() == false)
+	/*아주아주 중요한 개념*/
+	PKT_S_TEST* pkt = reinterpret_cast<PKT_S_TEST*>(buffer);
+	/*버퍼의 있는 내용을 스택메모리에 복사해서 사용하는 것이아니라 버퍼주소의 바이트
+	데이터를 그대로 타입으로 캐스팅해서 사용한다는 개념이다. 가변데이터의 경우 Packet안에 있는 List데이터를
+	읽을 수 있게 도와주는 헬퍼클래스가 리스트 마저도 타입캐스팅으로 읽어 버린다. 복사가 사실상 Zero*/
+	if (pkt->IsValidePck() == false)
 		return;
 
 	//cout << "ID: " << id << " HP : " << hp << " ATT : " << attack << endl;
 
-	vector<PKT_S_TEST::BuffsListItem> buffs;
+	PKT_S_TEST::BuffsList buffs = pkt->GetBuffsList();
 
-	buffs.resize(pkt.buffsCount);
-	for (int32 i = 0; i < pkt.buffsCount; i++)
-		br >> buffs[i];
-
-	cout << "BufCount : " << pkt.buffsCount << endl;
-	for (int32 i = 0; i < pkt.buffsCount; i++)
+	cout << "BufCount : " << buffs.Count() << endl;
+	for (int32 i = 0; i < buffs.Count(); i++)
 	{
 		cout << "BufInfo : " << buffs[i].buffId << " " << buffs[i].remainTime << endl;
+	}
+
+	for (auto it = buffs.begin(); it != buffs.end(); ++it)
+	{
+		cout << "BufInfo : " << it->buffId << " " << it->remainTime << endl;
+	}
+
+	for (auto& buff : buffs)
+	{
+		cout << "BufInfo : " << buff.buffId << " " << buff.remainTime << endl;
 	}
 }
