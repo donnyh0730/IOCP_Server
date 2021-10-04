98c29afcf4863607c79626cec7af7e3eecea92d1
donnyh0730 <donnyh0730@gmail.com>
2021-10-04 18:24:59 +0900

    Including Google Protobuf

--- Server/DummyClient/ClientPacketHandler.cpp
+++ Server/DummyClient/ClientPacketHandler.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "ClientPacketHandler.h"
 #include "BufferReader.h"
-
+#include "Protocol.pb.h"
 void ClientPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 {
 	BufferReader br(buffer, len);
@@ -17,115 +17,26 @@ void ClientPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 	}
 }
 
-#pragma pack(1)//[1]Serialization을 하겠다는 컴파일러 명령어 
-
-// [ PKT_S_TEST ][BuffsListItem BuffsListItem BuffsListItem]
-struct PKT_S_TEST
-{
-	struct BuffsListItem
-	{
-		uint64 buffId;
-		float remainTime;
-
-		uint16 victimsOffset;
-		uint16 victimsCount;
-
-		bool IsValide(BYTE* packetStart, uint16 packetSize, OUT uint32& size)
-		{
-			if (victimsOffset + victimsCount * sizeof(uint64) > packetSize)
-				return false;
-
-			size += victimsCount * sizeof(uint64);
-			return true;
-		}
-	};
-
-	uint16 packetSize; // 공용 헤더
-	uint16 packetId; // 공용 헤더
-	uint64 id; // 8
-	uint32 hp; // 4
-	uint16 attack; // 2
-	uint16 buffsOffset;
-	uint16 buffsCount;
-
-	bool IsValidePck()
-	{
-		uint32 size = 0;
-		size += sizeof(PKT_S_TEST);
-		if (packetSize < size)
-			return false;
-
-		if (buffsOffset + buffsCount * sizeof(BuffsListItem) > packetSize)
-			return false;
-
-		// Buffers 가변 데이터 크기 추가
-		size += buffsCount * sizeof(BuffsListItem);
-
-		BuffsList buffList = GetBuffsList();
-		for (int32 i = 0; i < buffList.Count(); i++)
-		{
-			if (buffList[i].IsValide((BYTE*)this, packetSize, OUT size) == false)
-				return false;
-		}
-
-		// 최종 크기 비교
-		if (size != packetSize)
-			return false;
-
-		return true;
-	}
-
-	using BuffsList = PacketList<PKT_S_TEST::BuffsListItem>;
-	using BuffsVictimsList = PacketList<uint64>;
-
-	BuffsList GetBuffsList()
-	{
-		BYTE* data = reinterpret_cast<BYTE*>(this);
-		data += buffsOffset;
-		return BuffsList(reinterpret_cast<PKT_S_TEST::BuffsListItem*>(data), buffsCount);
-	}
-
-	BuffsVictimsList GetBuffsVictimList(BuffsListItem* buffsItem)
-	{
-		BYTE* data = reinterpret_cast<BYTE*>(this);
-		data += buffsItem->victimsOffset;
-		return BuffsVictimsList(reinterpret_cast<uint64*>(data), buffsItem->victimsCount);
-	}
-
-	//vector<BuffData> buffs;
-	//wstring name;
-};
-#pragma pack()
-
 void ClientPacketHandler::Handle_S_TEST(BYTE* buffer, int32 len)
 {
-	BufferReader br(buffer, len);
+	Protocol::S_TEST pkt;
 
-	/*아주아주 중요한 개념*/
-	PKT_S_TEST* pkt = reinterpret_cast<PKT_S_TEST*>(buffer);
-	/*버퍼의 있는 내용을 스택메모리에 복사해서 사용하는 것이아니라 버퍼주소의 바이트
-	데이터를 그대로 타입으로 캐스팅해서 사용한다는 개념이다. 가변데이터의 경우 Packet안에 있는 List데이터를
-	읽을 수 있게 도와주는 헬퍼클래스가 리스트 마저도 타입캐스팅으로 읽어 버린다. 복사가 사실상 Zero*/
-	if (pkt->IsValidePck() == false)
-		return;
+	ASSERT_CRASH(pkt.ParseFromArray(buffer + sizeof(PacketHeader), len - sizeof(PacketHeader)));
 
-	//cout << "ID: " << id << " HP : " << hp << " ATT : " << attack << endl;
+	cout << pkt.id() << " " << pkt.hp() << " " << pkt.attack() << endl;
 
-	PKT_S_TEST::BuffsList buffs = pkt->GetBuffsList();
+	cout << "BUFSIZE : " << pkt.buffs_size() << endl;
 
-	cout << "BufCount : " << buffs.Count() << endl;
-	for (auto& buff : buffs)
+	for (auto& buf : pkt.buffs())
 	{
-		cout << "BufInfo : " << buff.buffId << " " << buff.remainTime << endl;
-
-		PKT_S_TEST::BuffsVictimsList victims = pkt->GetBuffsVictimList(&buff);
-
-		cout << "Victim Count : " << victims.Count() << endl;
-
-		for (auto& victim : victims)
+		cout << "BUFINFO : " << buf.buffid() << " " << buf.remaintime() << endl;
+		cout << "VICTIMS : " << buf.victims_size() << endl;
+		for (auto& vic : buf.victims())
 		{
-			cout << "Victim : " << victim << endl;
+			cout << vic << " ";
 		}
 
+		cout << endl;
 	}
 }
+
